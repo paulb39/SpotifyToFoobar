261bb374452a74aff86f453f6d301397b8418ec8
paulb39 <paulbrenner1@gmail.com>
2020-07-03 16:57:32 -0400

    Init Release

--- SpotifyToFoobar/Form1.Designer.cs
+++ SpotifyToFoobar/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPlaylist = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPlaylist
+            // 
+            this.txtPlaylist.Location = new System.Drawing.Point(260, 143);
+            this.txtPlaylist.Name = "txtPlaylist";
+            this.txtPlaylist.Size = new System.Drawing.Size(280, 26);
+            this.txtPlaylist.TabIndex = 0;
+            this.txtPlaylist.Text = "Playlist id";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(605, 396);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(183, 42);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtPlaylist);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPlaylist;
+        private System.Windows.Forms.Button btnExport;
     }
 }
 
--- SpotifyToFoobar/Form1.cs
+++ SpotifyToFoobar/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,74 @@ namespace SpotifyToFoobar
 {
     public partial class Form1 : Form
     {
+        private SpotifyWebAPI _spotifyAPI { get; set; }
+        private String _clientId { get; set; }
+        private List<FullTrack> _tracks { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            _clientId = @"YOUR SPOTIFY DEV ID HERE";
+            _tracks = new List<FullTrack>();
         }
+
+        private async void btnExport_Click(object sender, EventArgs e)
+        {
+            var auth = new ImplicitGrantAuth(
+                _clientId,
+                "http://localhost:4002",
+                "http://localhost:4002",
+                Scope.UserLibraryModify | Scope.PlaylistModifyPrivate
+            );
+
+            auth.Start();
+            auth.OpenBrowser();
+
+            auth.AuthReceived += (s, payload) =>
+            {
+                auth.Stop();
+                _spotifyAPI = new SpotifyWebAPI()
+                {
+                    TokenType = payload.TokenType,
+                    AccessToken = payload.AccessToken
+                };
+
+                int page = 0;
+                bool load = true;
+
+                while (load)
+                {
+                    Paging<PlaylistTrack> playlist = _spotifyAPI.GetPlaylistTracks(txtPlaylist.Text, null, 100, page);
+                    if (!playlist.HasNextPage())
+                    {
+                        load = false;
+                    }
+                    
+
+                    foreach (var track in playlist.Items)
+                    {
+                        _tracks.Add(track.Track);
+                    }
+
+                    page += 100;
+                }
+
+                using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter(@"SpotifyPlaylist.m3u", true))
+                {
+                    foreach (var song in _tracks)
+                    {
+                        Console.WriteLine(song.Uri);
+                        file.WriteLine(song.Uri);
+                    }
+                }
+
+                Console.WriteLine("DONE");
+            };
+
+
+        }
+
+
     }
 }
